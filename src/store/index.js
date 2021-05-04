cccc402ab8a51b61d677f250a00d5683818003cf
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-05-04 14:03:08 -0400

    use redux-thunk with api

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 // Importe o método applyMiddleware
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 // Importe o redux-thunk
 import rootReducer from '../reducers';
 
 // Aplique o middleware
 const store = createStore(
   rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 export default store;
